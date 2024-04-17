c634a1755d3b096006047ecdbb5047e01ec35cfc
anonyxbiz <anonyxbiz@gmail.com>
2024-04-17 06:59:51 +0300

    update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 from cryptography.fernet import Fernet
 from json import load, dump
+from random import randint as rd
 
 p = print
 
@@ -10,16 +11,16 @@ def spotify(user):
 def user_dashboard(user):
     user_data = {
         "username": user,
-        "analytics": [200, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95],
+        "analytics": [rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), 90, 150, 160, 120, 140, rd(200,10000), rd(200,10000)],
         "analytics_labels": ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
         "analytics_label": "Streams",
         "email_stats_label": "Stats",
         "email_stats_labels": ["12pm", "3pm", "6pm", "9pm", "12am", "3am", "6am", "9am"],
-        "email_stats": [40, 500, 650, 700, 1200, 1250, 1300, 1900],
-        "latest_releases_data": [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630],
+        "email_stats": [rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000)],
+        "latest_releases_data": [rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000), rd(200,10000)],
         "latest_releases_labels": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
         "latest_releases_label": "release_name",
-        "statements": "<tr><td>30/03/2024</td><td>January</td><td>$36,738</td><td class=\"text-right\">Processing</td></tr>"
+        "statements": "<tr><td>30/03/2024</td><td>January</td><td>${},{}</td><td class=\"text-right\">Processing</td></tr>".format(rd(1,100), rd(100,1000))
     }
     return user_data
 
