f6d74bfcc7a21edcf730eba6397335730d874d43
anonyxbiz <anonyxbiz@gmail.com>
2024-04-17 06:29:37 +0300

    update demo.js

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -2,84 +2,60 @@ let user_data = "";
 let shipped_products = "";
 
 // const loadDataFromLocalstorage = async () => 
-function chart_user_data() {
-    const user_data = fetch('/api/v1/analytics/artist', {
-        method: 'POST',
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'username': "username",
-        })
-
-    }).json();
-    
-    const user_data_old = {
-        "username": "breeder_lw",
-        "analytics": [200, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95],
-        "analytics_labels": ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
-        "analytics_label": "Streams",
-        "email_stats_label": "Stats",
-        "email_stats_labels": ["12pm,", "3pm", "6pm", "9pm", "12am", "3am", "6am", "9am"],
-        "email_stats": [40, 500, 650, 700, 1200, 1250, 1300, 1900],
-        "latest_releases_data": [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630],
-        "latest_releases_labels": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        "latest_releases_label": "release_name",
-        "statements": "<tr><td>30/03/2024</td><td>January</td><td>$36,738</td><td class=\"text-right\">Processing</td></tr>"
-    }
-    return user_data;
-}
-
-async function user_analytics(username) {
-    response = await fetch('/api/v1/analytics/artist', {
-        method: 'POST',
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'username': username,
-        })
-
-    });
-
+async function chart_user_data() {
     try {
-        if (response.ok) {
-            const userdata = await response.json();
-            const shipped_products = userdata;
-            return shipped_products;
-            
+        const response = await fetch('https://sought-casual-moth.ngrok-free.app/api/v1/analytics/artist', {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'username': "username",
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
         }
-    }
-    catch (e) {
-        console.log(e);
-    }
 
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        // If there's an error fetching data, return the backup data
+        return {
+            "username": "breeder_lw",
+            "analytics": [200, 150, 100, 190, 130, 90, 150, 160, 120, 140, 190, 95],
+            "analytics_labels": ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+            "analytics_label": "Streams",
+            "email_stats_label": "Stats",
+            "email_stats_labels": ["12pm,", "3pm", "6pm", "9pm", "12am", "3am", "6am", "9am"],
+            "email_stats": [40, 500, 650, 700, 1200, 1250, 1300, 1900],
+            "latest_releases_data": [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630],
+            "latest_releases_labels": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            "latest_releases_label": "release_name",
+            "statements": "<tr><td>30/03/2024</td><td>January</td><td>$36,738</td><td class=\"text-right\">Processing</td></tr>"
+        };
+    }
 }
 
-const u_a_a = async () => {
+async function loadData() {
     try {
-        const username = "breeder_lw";
-        user_analytics(username);
-        
-        user_data = chart_user_data();
+        // Fetch data from the API
+        user_data = await chart_user_data();
+        console.log(user_data);
 
+        // Update HTML content with the fetched data
+        var statements = document.getElementById("statements");
+        statements.innerHTML = user_data.statements;
     } catch (e) {
-        console.log(e)
+        // Handle errors
+        console.error(e);
     }
 }
 
-user_data = chart_user_data();
-console.log(user_data);
-
-try {
-    console.log(user_data);
-    var statements = document.getElementById("statements");
-    statements.innerHTML = user_data.statements;
-} catch (e) {
-    throw e
-}
+// Call the async function to load data
+loadData();
 
 demo = {
     initPickColor: function() {
